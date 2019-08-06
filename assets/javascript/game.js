d88d7020a6d52c73e8cd93a903f0b4bc8daedc4d
Sonny D <damicose14@gmail.com>
2019-08-06 16:14:26 -0500

    initial java coding, rng for goal functioning confirmed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,14 @@
+let goalTotal = null
+let userTotal = 0
+let wins = 0
+let losses = 0
+
+let goal = 19 + Math.floor(Math.random() * ((120 - 19) + 1))
+
+console.log(goal)
+
+
+//Min + (int)(Math.random() * ((Max - Min) + 1))
 // Assign random value [1-12] to each crystal button
 
 //Assign random value to goal # [19-120]
