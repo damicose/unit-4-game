cc968c8071931033b32030ee906935287b6c5199
Sonny D <damicose14@gmail.com>
2019-08-06 15:33:30 -0500

    psuedo coded out basic framework for each file type

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # unit-4-game
+
+https://damicose.github.io/unit-4-game/
---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Assign random value [1-12] to each crystal button
+
+//Assign random value to goal # [19-120]
+
+//Click listener for each crystal button 
+
+//Clicking crystal adds value to total
+
+//Win condition: userTotal = goalTotal; Lose if userTotal > goalTotal
+
+//Game resets and reassigns all values upon W/L
