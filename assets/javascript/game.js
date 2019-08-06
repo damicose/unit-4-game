00ea1a7510d536e7ac5214851615eab7e5f5b511
Sonny D <damicose14@gmail.com>
2019-08-06 17:58:37 -0500

    buttons are functional and scoring correctly, can't get win/lose to function. Overlooking something

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,31 +14,59 @@ let crysVal4 = 1 + Math.floor(Math.random() * ((12 - 1) + 1))
 //Assign random value to goal # [19-120]
 let goal = 19 + Math.floor(Math.random() * ((120 - 19) + 1))
 
-function
+function updateGoalTotal() {
+    document.getElementById("goalTotal").innerHTML = "Total to win: " + goal
+}
 
+const reset = function () {
+    crysVal1 = 1 + Math.floor(Math.random() * ((12 - 1) + 1))
+    crysVal2 = 1 + Math.floor(Math.random() * ((12 - 1) + 1))
+    crysVal3 = 1 + Math.floor(Math.random() * ((12 - 1) + 1))
+    crysVal4 = 1 + Math.floor(Math.random() * ((12 - 1) + 1))
+    goal = 19 + Math.floor(Math.random() * ((120 - 19) + 1))
+    updateGoalTotal()
+}
+
+function updateCurrentScore() {
+    document.getElementById("userTotal").innerHTML = "Current Score: " + userTotal
+}
+
+reset();
 
 //Click listener for each crystal button 
 //Clicking crystal adds value to total
 
 document.getElementById("crys1").addEventListener("click", function () {
     userTotal = userTotal + crysVal1;
+    updateCurrentScore();
 });
 document.getElementById("crys2").addEventListener("click", function () {
     userTotal = userTotal + crysVal2;
+    updateCurrentScore();
 });
 document.getElementById("crys3").addEventListener("click", function () {
     userTotal = userTotal + crysVal3;
+    updateCurrentScore();
 });
 document.getElementById("crys4").addEventListener("click", function () {
     userTotal = userTotal + crysVal4;
+    updateCurrentScore();
 });
 
 //Win condition: userTotal = goalTotal; Lose if userTotal > goalTotal
-if (usertotal > goal) {
-alert("You lose!")
+if (userTotal > goalTotal) {
+    losses++;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    alert("You lose!");
+    reset();
+    updateGoalTotal();
 }
 
-else if (userTotal === goal) {
-alert("You win!")
+else if (userTotal == goalTotal) {
+    wins++;
+    alert("You win!");
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    reset();
+    updateGoalTotal();
 }
 //Game resets and reassigns all values upon W/L
